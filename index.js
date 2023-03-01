b9b4e81561332e77a6344b9b438fc1eee23851ad
h16k <95564547+h16k@users.noreply.github.com>
2023-03-01 14:56:59 +0900

    fix

--- index.js
+++ index.js
@@ -1,28 +1,55 @@
 const maxHeight = window.screen.availHeight;
 
-function changeSheetColor(colorname){
-  var obj = document.getElementById('anki-sheet');
-  obj.style.backgroundColor=colorname;
+function changeSheetColor(colorname) {
+    var obj = document.getElementById('anki-sheet');
+    obj.style.backgroundColor = colorname;
 
 }
 
-function toggleHidden(idname){
+function spoit() {
+    const eyeDropper = new EyeDropper();
+
+    eyeDropper
+      .open()
+      .then((colorSelectionResult) => {
+        changeSheetColor(colorSelectionResult.sRGBHex);
+      })
+      .catch((error) => {
+        // キャンセルした場合の例: `DOMException: The user canceled the selection.`
+        console.error(error);
+      });
+
+}
+
+function toggleHidden(idname) {
     var obj = document.getElementById(idname);
     obj.classList.toggle("hidden")
 }
 
-function sheetUp(){
+function sheetUp() {
     var obj = document.getElementById('anki-sheet');
     height = obj.offsetHeight;
-    if(height+70<maxHeight){
-        obj.style.height = height+70 + "px";
+    if (height + 70 < maxHeight) {
+        obj.style.height = height + 70 + "px";
     }
 }
 
-function sheetDown(){
+function sheetDown() {
     var obj = document.getElementById('anki-sheet');
     height = obj.offsetHeight;
-    if(height-70>50){
-        obj.style.height = height-70 + "px";
+    if (height - 70 > 50) {
+        obj.style.height = height - 70 + "px";
     }
+}
+
+function loadMaterial() {
+    let material = document.getElementById("materialURI").value;
+    document.getElementById('material').innerHTML = `<iframe src="${material}" frameborder="0"></iframe> `;
+}
+
+function enter(e) {
+    if (e === 'Enter') {
+        loadMaterial();
+    }
+    return false;
 }
